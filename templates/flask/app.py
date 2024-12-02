f4a4f686e7199d5e99aabb77505d56b30e899cc8
John Rocha <john.rocha.pt@gmail.com>
2024-12-02 17:20:46 -0500

    Git Demo 1

--- templates/flask/app.py
+++ templates/flask/app.py
@@ -1,8 +1,13 @@
 from flask import Flask, render_template, request
 
 app = Flask(__name__)
+app = Flask(__name__, static_folder='static', static_url_path='/', template_folder='templates')
+app.secret_key = 'K5QwCWsJ8qkypg2R3rifHnI0O7'
 
 #: Use the route() decorator to bind a function to a URL.
 @app.route("/")
 def index():
     return render_template("index.html")
+
+if __name__ == '__main__':
+    app.run(debug=True)
